111d41a164a69c31b1c41b121ea2088003c1ba3d
dura0ok <slpmcf@gmail.com>
2025-03-09 11:38:10 +0700

    transfer methods to cpp file

--- raft_server.cpp
+++ raft_server.cpp
@@ -1,5 +1,27 @@
-//
-// Created by durachok on 3/9/25.
-//
-
 #include "raft_server.h"
+
+grpc::Status RaftServiceImpl::RequestForVote(grpc::ServerContext *context,
+                                             const raft_protocol::RequestVoteRequest *request,
+                                             raft_protocol::RequestVoteResponse *response)
+{
+    std::cout << "Received RequestForVote: term = " << request->term() << ", candidateId = " << request->candidateid()
+              << std::endl;
+
+    response->set_term(1);
+    response->set_votegranted(true);
+
+    return grpc::Status::OK;
+}
+
+grpc::Status RaftServiceImpl::AppendEntries(grpc::ServerContext *context,
+                                            const raft_protocol::AppendEntriesRequest *request,
+                                            raft_protocol::AppendEntriesResponse *response)
+{
+    std::cout << "Received AppendEntries: term = " << request->term() << ", leaderId = " << request->leaderid()
+              << std::endl;
+
+    response->set_term(1);
+    response->set_success(true);
+
+    return grpc::Status::OK;
+}
--- raft_server.h
+++ raft_server.h
@@ -3,38 +3,20 @@
 
 #include "proto/raft.grpc.pb.h"
 #include "proto/raft.pb.h"
-#include <grpcpp/grpcpp.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
 
-
-class RaftServiceImpl final : public raft_protocol::RaftService::Service {
-public:
-    grpc::Status RequestForVote(grpc::ServerContext* context,
-                                const raft_protocol::RequestVoteRequest* request,
-                                raft_protocol::RequestVoteResponse* response) override {
-        std::cout << "Received RequestForVote: term = " << request->term()
-                  << ", candidateId = " << request->candidateid() << std::endl;
-
-        response->set_term(1);
-        response->set_votegranted(true);
-
-        return grpc::Status::OK;
-    }
-
-    grpc::Status AppendEntries(grpc::ServerContext* context,
-                               const raft_protocol::AppendEntriesRequest* request,
-                               raft_protocol::AppendEntriesResponse* response) override {
-        std::cout << "Received AppendEntries: term = " << request->term()
-                  << ", leaderId = " << request->leaderid() << std::endl;
-
-        response->set_term(1);
-        response->set_success(true);
-
-        return grpc::Status::OK;
-    }
+class RaftServiceImpl final : public raft_protocol::RaftService::Service
+{
+  public:
+    grpc::Status RequestForVote(grpc::ServerContext *context, const raft_protocol::RequestVoteRequest *request,
+                                raft_protocol::RequestVoteResponse *response) override;
+    grpc::Status AppendEntries(grpc::ServerContext *context, const raft_protocol::AppendEntriesRequest *request,
+                               raft_protocol::AppendEntriesResponse *response) override;
 };
 
-inline void RunServer() {
+inline void RunServer()
+{
     std::string server_address("0.0.0.0:50051");
     RaftServiceImpl service;
 
@@ -45,12 +27,10 @@ inline void RunServer() {
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
 
-    grpc::Server* server = builder.BuildAndStart().release();
+    grpc::Server *server = builder.BuildAndStart().release();
 
     std::cout << "Server listening on " << server_address << std::endl;
     server->Wait();
 }
 
-
-
-#endif //RAFTSERVER_H
+#endif // RAFTSERVER_H
